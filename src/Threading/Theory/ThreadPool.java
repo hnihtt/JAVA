01ec35d23021c9049b8b1c576d122f87badd1450
VinhThinh <vinhthinhvo@gmail.com>
2026-04-28 12:04:47 +0700

    add some more examples with thread pool

--- src/Threading/Theory/ThreadPool.java
+++ src/Threading/Theory/ThreadPool.java
@@ -1,20 +1,44 @@
 package Threading.Theory;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class ThreadPool {
-    public static void main(String[] args) {
-        Runnable task1 = () -> System.out.println("Task 1");
-        Runnable task2 = () -> System.out.println("Task 2");
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Thread task1 = new Thread(() -> System.out.println("Task1"));
+        Thread task2 = new Thread(() -> System.out.println("Task2"));
+        task1.start();
+        task2.start();
+
+
         Runnable task3 = () -> System.out.println("Task 3");
+        Runnable task4 = () -> System.out.println("Task 4");
+        Runnable task5 = () -> System.out.println("Task 5");
 
         ExecutorService pool = Executors.newFixedThreadPool(3);
 
-        pool.execute(task1);
-        pool.execute(task2);
+        pool.execute(() -> System.out.println("X"));
         pool.execute(task3);
+        pool.execute(task4);
+        pool.execute(task5);
         pool.shutdown();
+
+
+        ExecutorService poolFuture = Executors.newFixedThreadPool(2);
+
+        Callable<Integer> task = () -> {
+            Thread.sleep(5000);
+            return 99;
+        };
+
+        Future<Integer> future = poolFuture.submit(task);
+
+        System.out.println("Working...");
+
+        Integer result = future.get();
+
+        System.out.println("Result: " + result);
+
+        poolFuture.shutdown();
+
     }
 }
